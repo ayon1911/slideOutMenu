b1aea58caf28de5494790b19abf93c9856defaa3
ayon <kr_ayon@icloud.com>
2018-10-14 17:25:06 +0200

    velocity for the gesture

--- SlideOutMEnu/Controllers/HomeController.swift
+++ SlideOutMEnu/Controllers/HomeController.swift
@@ -12,11 +12,17 @@ class HomeController: UITableViewController {
     //variables
     let sliderVC = SliderController()
     fileprivate let menuWidth: CGFloat = 300
+    fileprivate let velocityThreshold: CGFloat = 500
+    fileprivate var sliderOpened = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.backgroundColor = .red
         setupNavigation()
+        setupSliderController()
+        
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePan))
+        view.addGestureRecognizer(panGesture)
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -28,23 +34,61 @@ class HomeController: UITableViewController {
         
         return cell
     }
-    
+    //MARK:- controller handler
     @objc func handleOpen() {
-        
-        sliderVC.view.frame = CGRect(x: -menuWidth, y: 0, width: menuWidth, height: self.view.frame.height)
-        let mainWindow = UIApplication.shared.keyWindow
-        mainWindow?.addSubview(sliderVC.view)
         //sliding out---
-        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
-            self.sliderVC.view.transform = CGAffineTransform(translationX: self.menuWidth, y: 0)
-        })
-        addChild(sliderVC)
+        sliderOpened = true
+        performAnimation(trasnform: CGAffineTransform(translationX: self.menuWidth, y: 0))
     }
+    
     @objc func handleHide() {
         //sliding in--
-        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
-            self.sliderVC.view.transform = .identity
-        })
+        sliderOpened = false
+        performAnimation(trasnform: .identity)
+    }
+    
+    @objc func handlePan(gesture: UIPanGestureRecognizer) {
+        let traslation = gesture.translation(in: view)
+        if gesture.state == .changed {
+            var varingX = traslation.x
+            if sliderOpened {
+                varingX += menuWidth
+            }
+            varingX = min(menuWidth, varingX)
+            varingX = max(0, varingX)
+            let transformX = CGAffineTransform(translationX: varingX, y: 0)
+            sliderVC.view.transform = transformX
+            navigationController?.view.transform = transformX
+        } else if gesture.state == .ended {
+            handleEnded(gesture: gesture)
+        }
+    }
+    
+    //MARK:- fileprivate Functions
+    fileprivate func handleEnded(gesture: UIPanGestureRecognizer) {
+        let traslation = gesture.translation(in: view)
+        let velocity = gesture.velocity(in: view)
+        if sliderOpened {
+            if abs(velocity.x) > velocityThreshold {
+                handleHide()
+                return
+            }
+            if abs(traslation.x) < menuWidth / 2 {
+                handleOpen()
+            } else {
+                handleHide()
+            }
+        } else {
+            if velocity.x > velocityThreshold {
+                handleOpen()
+                return
+            }
+            if traslation.x < menuWidth / 2 {
+                handleHide()
+            } else {
+                handleOpen()
+            }
+        }
     }
     
     fileprivate func setupNavigation() {
@@ -52,6 +96,20 @@ class HomeController: UITableViewController {
         navigationItem.title = "Home"
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(handleOpen))
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Hide", style: .plain, target: self, action: #selector(handleHide))
+    }
+    
+    fileprivate func setupSliderController() {
+        sliderVC.view.frame = CGRect(x: -menuWidth, y: 0, width: menuWidth, height: self.view.frame.height)
+        let mainWindow = UIApplication.shared.keyWindow
+        mainWindow?.addSubview(sliderVC.view)
+        addChild(sliderVC)
+    }
+    
+    fileprivate func performAnimation(trasnform: CGAffineTransform) {
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+            self.sliderVC.view.transform = trasnform
+            self.navigationController?.view.transform = trasnform
+        })
     }
 }
 
