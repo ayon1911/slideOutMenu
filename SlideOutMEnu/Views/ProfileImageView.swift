65cd19abf42f41a397ca7fce9545486e2b582f38
ayon <kr_ayon@icloud.com>
2018-10-24 12:54:27 +0200

    ProfileImageView added

---
+++ SlideOutMEnu/Views/ProfileImageView.swift
@@ -0,0 +1,21 @@
+//
+//  ProfileImageView.swift
+//  SlideOutMEnu
+//
+//  Created by Khaled Rahman Ayon on 24.10.18.
+//  Copyright © 2018 DocDevs. All rights reserved.
+//
+
+import UIKit
+
+class ProfileImageView: UIImageView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
